0dd7ddf16e8b771cfc1cf8198962a7890e249802
Jordan Epps <jordanepps@gmail.com>
2020-03-15 18:07:41 -0400

    Add frontend components for image upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+import axios from 'axios';
+
+import ImageUploader from './components/ImageUploader';
+
 function App() {
+  const [files, setFiles] = useState([]);
+
+  const handleUpload = async () => {
+    const formData = new FormData();
+    formData.append('file', files[0]);
+
+    try {
+      const res = await axios.post('/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+      // const { fileName, filePath } = res.data;
+      console.log(res.data);
+    } catch (err) {
+      if (err.response.status === 500) {
+        console.log(err);
+      } else {
+        console.log(err);
+      }
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Image Recipe</h1>
+      <ImageUploader files={files} setFiles={setFiles} />
+      <button onClick={handleUpload} disabled={!files.length}>
+        Upload
+      </button>
     </div>
   );
 }
