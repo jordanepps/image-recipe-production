c850b584cee14e5d1f75809e770d9c7c9e708913
Jordan Epps <jordanepps@gmail.com>
2020-03-17 08:44:33 -0400

    Change file to base64 string

--- src/routes/upload-router.js
+++ src/routes/upload-router.js
@@ -1,16 +1,25 @@
 const express = require('express');
+const fs = require('fs');
 
 const uploadRouter = express.Router();
 const jsonBodyParser = express.json();
+
+function base64_encode(data) {
+  // convert binary data to base64 encoded string
+  return new Buffer(data).toString('base64');
+}
 
 uploadRouter.route('/').post(jsonBodyParser, (req, res, next) => {
   //Check if a file is uploaded
   if (!req.files) return res.status(400).json({ msg: 'No file uploaded' });
 
   const file = req.files.file;
-  //   console.log(file);
+  //   console.log('TEST', fs.readFileSync('package.json'));
+  //   console.log('FILE', file.data);
 
-  res.json({ msg: 'done' });
+  const bytes = base64_encode(file.data);
+
+  res.json({ msg: 'done', bytes });
 });
 
 module.exports = uploadRouter;
